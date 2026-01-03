981d59d778fedb08eeeb40b856942239cfd8e316
Noy Cohen <Noycohen240@gmail.com>
2026-01-03 21:46:05 +0200

    fix code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const header = document.querySelector(".menu_list");
-const themeToggle = document.querySelector("theme_toggle");
+const themeToggle = document.querySelector(".theme_toggle");
 
 hamburger.addEventListener("click", function () {
     header.classList.toggle("nav-open");
@@ -12,9 +12,9 @@ themeToggle.addEventListener("click", function () {
     document.body.classList.toggle("dark");
 
     if (document.body.classList.contains("dark")){
-        themeToggle.textContent = "☀️";
+        themeToggle.textContent = "Light Mode";
     }
     else{
-        themeToggle.textContent = "🌙";
+        themeToggle.textContent = "Dark Mode";
     }
 });
