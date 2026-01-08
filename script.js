de7f9c23c25d759ecf11640daa092ec04ad5d957
Noy Cohen <Noycohen240@gmail.com>
2026-01-08 18:06:13 +0200

    update

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const header = document.querySelector(".menu_list");
 
 hamburger.addEventListener("click", function () {
     header.classList.toggle("active");
-
+    document.body.classList.toggle("menu_open")
 });
 
