73457efa6f7843153383d53604c8bd1ddf944a15
Ohad Posti <opostibu@gmail.com>
2026-01-15 18:35:05 +0200

    continue

--- supportform.js
+++ supportform.js
@@ -1,37 +1,15 @@
-// הקישור שלך מגוגל - ניקיתי אותו מתווים מיותרים
-const scriptURL = 'https://script.google.com/macros/s/AKfycbz0g2QX3NWh0T1gTQkEZrGtrOboQjcx-8JgFlZO8N8cYWqk6VxJpz0auuFgyTOAb9ln/exec';
+const form = document.getElementById('supportForm');
 
-const form = document.getElementById('supportForm' );
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  
-  // מציאת הכפתור בתוך הטופס
-  const submitBtn = form.querySelector('.submit-button');
-  
-  // מניעת לחיצות כפולות
-  submitBtn.disabled = true;
-  submitBtn.innerText = 'Sending...';
+    const userName = document.getElementById('user-name').value;
 
-  console.log("Sending data to Google Sheets...");
+    const successMessage = `Thank you, ${userName}!\n\nYour support request has been successfully received. It has been forwarded to our representative, who will contact you shortly.\n\nNo Matter The Weather, we are here for you!`;
+    
+    alert(successMessage);
 
-  // שליחת הנתונים
-  fetch(scriptURL, { 
-    method: 'POST', 
-    body: new FormData(form)
-  })
-  .then(response => {
-    console.log("Success!", response);
-    alert('Success! Your request has been saved to Excel.');
-    form.reset(); // ניקוי הטופס
-  })
-  .catch(error => {
-    console.error("Error!", error.message);
-    alert('Oops! Something went wrong: ' + error.message);
-  })
-  .finally(() => {
-    // החזרת הכפתור למצב רגיל
-    submitBtn.disabled = false;
-    submitBtn.innerText = 'Submit Request';
-  });
+    form.reset();
+    
+    console.log("Form submitted successfully (Visual only)");
 });
